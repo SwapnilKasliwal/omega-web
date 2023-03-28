504bbd9c68f4de61f1054be57b5882ef5ce918f9
KasliwalSwapnil <swapnil.kasliwal.int@hotstar.com>
2023-03-28 21:06:47 +0530

    about page addition

---
+++ src/Components/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from './Footer/Footer'
+export default function AboutPage() {
+  return (
+    <div>
+      <div>
+        This is an about page
+      </div>
+      <Footer/>
+    </div>
+    
+  )
+}
